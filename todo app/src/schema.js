8eb93afe5e794a451b84cedf63b5a8175a0c4236
Erik Le <erik.le@outlook.com>
2025-03-29 16:21:19 +0100

    db update + rename  ukol_4 -> todo app

---
+++ todo app/src/schema.js
@@ -0,0 +1,12 @@
+import {
+    sqliteTable,
+    int,
+    text,
+  } from "drizzle-orm/sqlite-core"
+  
+  export const todosTable = sqliteTable("todos", {
+    id: int().primaryKey({ autoIncrement: true }),
+    title: text().notNull(),
+    completed: int({ mode: "boolean" }).notNull(),
+  })
+  
